912e6df9852813742d375983963c6762232a72ba
trasterlabs <78925138+trasterlabs@users.noreply.github.com>
2021-08-30 16:17:41 +0200

    Set Start Line instruction functions

--- inc/ST7567_Protocol.h
+++ inc/ST7567_Protocol.h
@@ -27,4 +27,44 @@
 #define INC_ST7567_PROTOCOL_H_
 
 
+/** @addtogroup ST7567_Protocol_Driver Protocol driver for ST7567
+  * @brief All the protocol parts for the ST7567 serial screen module
+  * @{
+  */
+
+/** @addtogroup ST7567_Protocol_set_start_line ST7567 Set Start Line
+  * @{
+  * |    INSTRUCTION     |
+  * |:-------------------|
+  * | (2) Set Start Line |
+  * This instruction sets the line address of the Display Data RAM to determine the initial display line. The display data of the
+  * specified line address is displayed at the top row (COM0) of the LCD panel.
+  */
+
+/**
+ * @brief Initiates the instruction "Set Start Line"
+ */
+void init_set_start_line_instruction ( void );
+
+/**
+ *  \brief Adjust the line address for the instruction Set Start Line
+ *  
+ *  \param [in] line_address The specified line address. It must be from 0 to 63
+ */
+void set_line_address_for_set_start_line_instruction ( uint8_t line_address );
+
+/**
+ *  \brief Send the instruction Set Start Line
+ */
+void send_set_start_line_instruction ( void );
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
+
 #endif /* INC_ST7567_PROTOCOL_H_ */
