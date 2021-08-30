ef681d1247634722d4f538a13f8ea683161d183f
trasterlabs <78925138+trasterlabs@users.noreply.github.com>
2021-08-30 15:26:19 +0200

    Modified the copyright part

--- inc/ST7567_Protocol.h
+++ inc/ST7567_Protocol.h
@@ -1,18 +1,30 @@
-/*
- *
- * Trasfile: ST7657_Protocol.h
- *
- * All rights reserved.
- *
- * All the text and images presented here belongs to Trasterlabs, only for portfolio purposes.
- *
- * Do not use in your projects without permission.
- *
- * ¡Rompe poco, pero trastea todo lo que puedas!
- *
- */
+/**
+  ******************************************************************************
+  * @file    ST7657_Protocol.h
+  * @author  Trasterlabs
+  * @brief   This file contains all the functions prototypes for the serial GLCD
+  * layer using ST7567 protocol
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2021 Trasterlabs.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by Trasterlabs. You shall not use this
+  * file without permission of the author.
+  *
+  * All the text and images presented here belongs to Trasterlabs, only for
+  * portfolio purposes.
+  *
+  * Do not use in your projects without permission.
+  *
+  * ¡Rompe poco, pero trastea todo lo que puedas!
+  *
+  ******************************************************************************
+  */
 
 #ifndef INC_ST7567_PROTOCOL_H_
 #define INC_ST7567_PROTOCOL_H_
+
 
 #endif /* INC_ST7567_PROTOCOL_H_ */
