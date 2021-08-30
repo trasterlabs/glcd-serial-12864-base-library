3e10ec40942a657a0da3d376175c03fdfe20deae
trasterlabs <78925138+trasterlabs@users.noreply.github.com>
2021-08-30 23:33:15 +0200

    Update ST7567_Protocol.c

--- src/ST7567_Protocol.c
+++ src/ST7567_Protocol.c
@@ -1,16 +1,27 @@
-/*
- *
- * Trasfile: ST7567_Protocol.c
- *
- * All rights reserved.
- *
- * All the text and images presented here belongs to Trasterlabs, only for portfolio purposes.
- *
- * Do not use in your projects without permission.
- *
- * ¡Rompe poco, pero trastea todo lo que puedas!
- *
- */
+/**
+  ******************************************************************************
+  * @file    ST7657_Protocol.c
+  * @author  Trasterlabs
+  * @brief   This file contains all the functions definitions for the serial
+  * GLCD layer using ST7567 protocol
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2021 Trasterlabs.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by Trasterlabs. You shall not use this
+  * file without permission of the author.
+  *
+  * All the text and images presented here belongs to Trasterlabs, only for
+  * portfolio purposes.
+  *
+  * Do not use in your projects without permission.
+  *
+  * ¡Rompe poco, pero trastea todo lo que puedas!
+  *
+  ******************************************************************************
+  */
 
 #include <stdint.h>
 #include "communication_stm32f103_SPI.h"
